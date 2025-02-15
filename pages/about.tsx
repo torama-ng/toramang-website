92b94b4964743d52a919c18926406cde9fc98b06
Akpodigha Filatei <filatei@gtsng.com>
2025-02-15 10:25:45 +0100

    added github actions

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,8 +10,8 @@ export default function About() {
       <main className="container mx-auto p-6 mt-20">
         <h1 className="text-4xl font-bold mb-4">About Us</h1>
         <p className="text-lg">
-          Torama is a leading technology company specializing in estate
-          management, fintech, and trading solutions.
+          Torama is a leading technology company specializing in developing apps
+          for estate management, fintech, and trading solutions.
         </p>
       </main>
       <Footer />
